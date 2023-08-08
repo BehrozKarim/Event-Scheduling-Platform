c4f194ea554820ac08bdc3143649cae63ee43de8
BehrozKarim <behrozbuzdar@gmail.com>
2023-08-09 01:27:43 +0500

    feat(User and Events): Functionality of Register User, Login, Logout, Edit Event, View Events are developed

--- event_scheduling_platform/event/models.py
+++ event_scheduling_platform/event/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rest_framework import reverse
 
 class Event(models.Model):
+    EVENT_TYPES = [
+        ('conference', 'Conference'),
+        ('webinar', 'Webinar'),
+        ('meetup', 'Meetup'),
+    ]
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=200)
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=EVENT_TYPES, default='conference')
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse.reverse('event-list')
+    
+    def created_updated(model, request):
+        obj = model.objects.latest('pk')
+        if obj.created_by is None:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()
+
 
 class Attendee(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ event_scheduling_platform/event/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('accounts/register/',views.register,name='register'),
+    path('event-list/', views.EventListView.as_view(), name='event-list'),
+    path('event/<int:pk>/edit/', views.EventUpdateView.as_view(), name='event-edit'),
+]
